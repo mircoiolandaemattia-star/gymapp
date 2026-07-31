0e19bfe6bd51bb708ae0ebda2cf06891d2fe085c
Mattia <mircoiolandaemattia@gmail.com>
2026-07-31 14:30:56 +0200

    Aggiungi .gitignore e rimuovi node_modules e dist dal tracking

--- dist/assets/plus-DJAMO-2v.js
+++
@@ -1,6 +0,0 @@
-/**
- * @license lucide v1.27.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"M5 12h14"}],["path",{d:"M12 5v14"}]];export{t as P};
